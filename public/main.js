95c513c991f0e2d140ea51d800d6a2a2cddf40c7
David Munoz Barco <david.f.do@hotmail.com>
2022-08-02 19:57:12 -0500

    test fetch

--- public/main.js
+++ public/main.js
@@ -3,3 +3,7 @@ const socket = io();
 socket.on('productos', data => {
     console.log("data cleinte: ",data);
 });
+
+fetch('http://localhost:8080/productos')
+.then(response => response.json())
+.then(data => console.log(data));
